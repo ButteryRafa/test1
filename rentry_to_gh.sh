da0032811e417697b66e90eaed9c253ad855409f
ButteryRafa <104410637+ButteryRafa@users.noreply.github.com>
2022-09-21 23:19:38 -0400

    Create rentry_to_gh.sh

---
+++ rentry_to_gh.sh
@@ -0,0 +1,38 @@
+then
+    output_file=$2
+else
+    output_file="rentry_to_gh_out.txt"
+fi
+
+cp $1 $output_file
+
+# Change admonitions
+# !!! note <info>
+# !!!note <info>
+# !!!note: <info>
+# to:
+# !!! note
+#     <info>
+# the extra \l makes the admonition's first letter lowercase
+sed -i -E 's/^\s*!!!\s*([[:alpha:]]+):*\s/!!! \l\1\n    /gi' $output_file
+
+# info did not have a colored box
+sed -i 's/!!! info/!!! note/gi' $output_file
+
+# Removed centered # and ##
+sed -i -E 's/^#\s*->(.*)\s*<-\s*$/# \1/g' $output_file
+sed -i -E 's/^##\s*->(.*)\s*<-\s*$/## -- \1/g' $output_file
+
+# Remove centered from images or links
+sed -i -E 's/\s*->\s*(!*\s*\[.*\]\s*\(.*\))\s*<-\s*/\1/g' $output_file
+
+# Replace ->&<- with <p align="center">&</p>
+sed -i 's/\s*->\s*/\<p align="center"\>/g; s/\s*<-\s*$/\<\/p\>/g' $output_file
+
+# Replace :-:, :--:, :---:, etc by :----:
+sed -i 's/:-*:/:----:/g' $output_file
+
+# Make Signets into Signet and then add a newline before signet tables
+sed -i 's/Signets | Priority/Signet | Priority/gi' $output_file
+sed -i 's/Signet | Priority/\n&/gi' $output_file
+
